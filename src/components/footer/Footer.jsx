259f6c32f6b03af7cbbb3ad2c05d44dbb2426cca
Baiel User <baiel@example.com>
2025-02-21 22:50:16 +0600

    Baiel

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='app-container  h-25 relative top-50  bg-white'>
+      <hr className="text-gray-500" />
+      <div className='w-62.5 m-auto relative top-6 pb-10'>
+        <img
+          src='https://the-mealdb-mimo.netlify.app/static/media/logo-mealDB.fa8d1d09f74f7c7f2a8b07b85e928f02.svg'
+          alt=''
+        />
+      </div>
+    </div>
   )
 }
 
